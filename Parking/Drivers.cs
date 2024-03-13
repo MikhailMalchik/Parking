255d8be4022a58062089b969f0a033f6ead6bcc8
misha <misha@DESKTOP-RIRAPN0>
2024-03-13 23:19:45 +0300

    #Add Drivers and ShoppingMalls

---
+++ Parking/Drivers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking
+{
+    public class Driver
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public List<CarsNumbers> CarsNumbers;
+
+        public Driver(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+            CarsNumbers = new List<CarsNumbers>();
+        }
+        public void AddCar(string number)
+        {
+            if(CarsNumbers.Any(x=>x.Number== number))
+            {
+                Console.WriteLine("Такой номер автомобиля уже существует");
+                return;
+            }
+            CarsNumbers.Add(new CarsNumbers(number));
+
+        }
+    }
+    public class CarsNumbers
+    {
+        public string Number { get; set; }
+        public CarsNumbers(string number)
+        {
+            Number = number;
+        }
+    }
+}
---
+++ Parking/Shoppingmalls.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking
+{
+    public class Mall
+    {
+        public string Name { get; set; }
+        public List<ParkingPlace> parkings;
+        public Mall(string n)
+        {
+            Name = n;
+            parkings = new List<ParkingPlace>();
+        }
+        public void AddParkingPlace(int capacity)
+        {
+            parkings.Add(new ParkingPlace(capacity));
+        }
+    }
+    public class ParkingPlace
+    {
+        public int Capacity { get; set; }
+        public ParkingPlace(int capacity )
+    {
+        Capacity = capacity;
+    }
+    }
+
+
+}
